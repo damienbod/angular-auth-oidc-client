e49e5fcec526fcd5113c182f085bc800d0f7bb19
damienbod <damien_bod@hotmail.com>
2017-08-10 23:31:41 +0200

    Adding a test

--- tests/services/oidc.security.service.spec.ts
+++ tests/services/oidc.security.service.spec.ts
@@ -61,6 +61,40 @@ describe('OidcSecurityService', () => {
             })
     );
 	
+	// https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-reference-oidc
+	it('createAuthorizeUrl with custom url like active-directory-b2c',
+        inject([OidcSecurityService],
+            (oidcSecurityService: OidcSecurityService) => {   
+			
+				let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+				openIDImplicitFlowConfiguration.stsServer = 'https://localhost:5001';
+				openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44386';
+				openIDImplicitFlowConfiguration.client_id = 'myid';
+				openIDImplicitFlowConfiguration.response_type = 'id_token token';
+				openIDImplicitFlowConfiguration.scope = 'openid email profile';
+				openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
+				openIDImplicitFlowConfiguration.startup_route = '/home';
+				openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+				openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+				openIDImplicitFlowConfiguration.start_checksession = false;
+				openIDImplicitFlowConfiguration.silent_renew = false;
+				openIDImplicitFlowConfiguration.log_console_warning_active = true;
+				openIDImplicitFlowConfiguration.log_console_debug_active = true;
+				openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
+				openIDImplicitFlowConfiguration.override_well_known_configuration = true;
+				openIDImplicitFlowConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
+
+				(oidcSecurityService as any).authConfiguration.init(openIDImplicitFlowConfiguration);
+				//oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+				
+				//oidcSecurityService.setCustomRequestParameters({'testcustom': 'customvalue', 'p': 'b2c_1_sign_in'});
+				
+                let value = (oidcSecurityService as any).createAuthorizeUrl('nonce', 'state', 'https://login.microsoftonline.com/fabrikamb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_sign_in');
+				let expectValue = 'https://login.microsoftonline.com/fabrikamb2c.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_sign_in&client_id=myid&redirect_uri=https://localhost:44386&response_type=id_token%20token&scope=openid%20email%20profile&nonce=nonce&state=state';
+                expect(expectValue).toEqual(value);
+            })
+    );
+	
 	it('createAuthorizeUrl with custom value',
         inject([OidcSecurityService],
             (oidcSecurityService: OidcSecurityService) => {   
@@ -94,4 +128,5 @@ describe('OidcSecurityService', () => {
             })
     );
 
+	
 });
