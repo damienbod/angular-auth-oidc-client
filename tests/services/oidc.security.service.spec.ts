12d57f4c7279d5bef0b9a440af331427a39067a6
damienbod <damien_bod@hotmail.com>
2017-08-10 20:11:45 +0200

    removing setup, test CI

--- tests/services/oidc.security.service.spec.ts
+++ tests/services/oidc.security.service.spec.ts
@@ -31,6 +31,9 @@ describe('OidcSecurityService', () => {
         inject([OidcSecurityService],
             (oidcSecurityService: OidcSecurityService) => {   
 			
+				// let well = '{ "issuer":"https://accounts.google.com", "authorization_endpoint":"https://accounts.google.com/o/oauth2/v2/auth", "token_endpoint":"https://www.googleapis.com/oauth2/v4/token", "userinfo_endpoint":"https://www.googleapis.com/oauth2/v3/userinfo", "revocation_endpoint":"https://accounts.google.com/o/oauth2/revoke", "jwks_uri":"https://www.googleapis.com/oauth2/v3/certs", "response_types_supported":[ "code", "token", "id_token", "codetoken", "codeid_token", "tokenid_token", "codetokenid_token", "none" ], "subject_types_supported":[ "public" ], "id_token_signing_alg_values_supported":[ "RS256" ], "scopes_supported":[ "openid", "email", "profile" ], "token_endpoint_auth_methods_supported":[ "client_secret_post", "client_secret_basic" ], "claims_supported":[ "aud", "email", "email_verified", "exp", "family_name", "given_name", "iat", "iss","locale","name","picture","sub"],"code_challenge_methods_supported":["plain","S256"]}';
+				// (oidcSecurityService as any).oidcSecurityCommon.store('wellknownendpoints', well);
+				
 				let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
 				openIDImplicitFlowConfiguration.stsServer = 'https://localhost:5001';
 				openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44386';
@@ -49,7 +52,8 @@ describe('OidcSecurityService', () => {
 				openIDImplicitFlowConfiguration.override_well_known_configuration = true;
 				openIDImplicitFlowConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
 
-				oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+				(oidcSecurityService as any).authConfiguration.init(openIDImplicitFlowConfiguration);
+				//oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
 		
 	
                 let value = (oidcSecurityService as any).createAuthorizeUrl('nonce', 'state', 'http://example');
@@ -79,8 +83,9 @@ describe('OidcSecurityService', () => {
 				openIDImplicitFlowConfiguration.override_well_known_configuration = true;
 				openIDImplicitFlowConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
 
-				oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
-		
+				(oidcSecurityService as any).authConfiguration.init(openIDImplicitFlowConfiguration);
+				//oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
+				
 				oidcSecurityService.setCustomRequestParameters({'testcustom': 'customvalue'});
 				
                 let value = (oidcSecurityService as any).createAuthorizeUrl('nonce', 'state', 'http://example');
