e15bc4b9bdfa17ace191c7d3c6aff40f160ebed2
Sam Lin <maxisam@gmail.com>
2017-10-10 09:32:28 -0500

    fix: import from specific location
    for #26

--- src/services/oidc.security.service.ts
+++ src/services/oidc.security.service.ts
@@ -4,7 +4,7 @@ import { Injectable, EventEmitter, Output } from '@angular/core';
 import { Http, Response, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Router } from '@angular/router';
 import { AuthConfiguration, OpenIDImplicitFlowConfiguration } from '../modules/auth.configuration';
