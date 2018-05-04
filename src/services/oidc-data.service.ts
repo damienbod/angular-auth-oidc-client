1df6a55818c8feef306ae9aa5d6ad30288770bc1
damienbod <damien_bod@hotmail.com>
2018-05-04 21:41:49 +0200

    updating to rxjs

--- src/services/oidc-data.service.ts
+++ src/services/oidc-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class OidcDataService {
