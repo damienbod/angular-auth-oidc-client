ed42c68d7c3aad46392bbb8286d2830396d58981
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-08-17 21:54:31 +0200

    added modulewithproviders again

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { AuthWellKnownEndpoints } from '../models/auth.well-known-endpoints';
 import { OidcDataService } from '../services/oidc-data.service';
 import { EqualityHelperService } from '../services/oidc-equality-helper.service';
@@ -10,48 +10,45 @@ import { OidcSecurityCommon } from '../services/oidc.security.common';
 import { OidcConfigService } from '../services/oidc.security.config.service';
 import { OidcSecurityService } from '../services/oidc.security.service';
 import { OidcSecuritySilentRenew } from '../services/oidc.security.silent-renew';
-import {
-    BrowserStorage,
-    OidcSecurityStorage,
-} from '../services/oidc.security.storage';
+import { BrowserStorage, OidcSecurityStorage } from '../services/oidc.security.storage';
 import { OidcSecurityUserService } from '../services/oidc.security.user-service';
 import { OidcSecurityValidation } from '../services/oidc.security.validation';
 import { AuthConfiguration, DefaultConfiguration } from './auth.configuration';
 
 @NgModule()
 export class AuthModule {
-    static forRoot(token: Token = {}) {
-        return {
-            ngModule: AuthModule,
-            providers: [
-                OidcConfigService,
-                OidcSecurityService,
-                OidcSecurityValidation,
-                OidcSecurityCheckSession,
-                OidcSecuritySilentRenew,
-                OidcSecurityUserService,
-                OidcSecurityCommon,
-                AuthConfiguration,
-                TokenHelperService,
-                LoggerService,
-                DefaultConfiguration,
-                EqualityHelperService,
-                AuthWellKnownEndpoints,
-                OidcDataService,
-                StateValidationService,
-                {
-                    provide: OidcSecurityStorage,
-                    useClass: token.storage || BrowserStorage,
-                },
-            ],
-        };
-    }
+  static forRoot(token: Token = {}): ModuleWithProviders {
+    return {
+      ngModule: AuthModule,
+      providers: [
+        OidcConfigService,
+        OidcSecurityService,
+        OidcSecurityValidation,
+        OidcSecurityCheckSession,
+        OidcSecuritySilentRenew,
+        OidcSecurityUserService,
+        OidcSecurityCommon,
+        AuthConfiguration,
+        TokenHelperService,
+        LoggerService,
+        DefaultConfiguration,
+        EqualityHelperService,
+        AuthWellKnownEndpoints,
+        OidcDataService,
+        StateValidationService,
+        {
+          provide: OidcSecurityStorage,
+          useClass: token.storage || BrowserStorage,
+        },
+      ],
+    };
+  }
 }
 
 export interface Type<T> extends Function {
-    new (...args: any[]): T;
+  new (...args: any[]): T;
 }
 
 export interface Token {
-    storage?: Type<any>;
+  storage?: Type<any>;
 }
