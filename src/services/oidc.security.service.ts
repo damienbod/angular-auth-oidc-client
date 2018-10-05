c1ef2eec2ca93c26c77e297840ea13d0f62b8d3d
damienbod <damien_bod@hotmail.com>
2018-10-05 23:08:03 +0200

    reset refreshsession, will fix in second PR

--- src/services/oidc.security.service.ts
+++ src/services/oidc.security.service.ts
@@ -86,7 +86,7 @@ export class OidcSecurityService {
                         )
                 ));
 
-                this.refreshSession();
+                //this.refreshSession();
 
                 return race$;
             }),
