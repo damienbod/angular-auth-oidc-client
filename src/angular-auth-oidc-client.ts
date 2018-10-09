42fdd6c8272a7e62d043359f268e644668697f39
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-10-09 09:56:31 +0200

    exported missing modules

--- src/angular-auth-oidc-client.ts
+++ src/angular-auth-oidc-client.ts
@@ -2,8 +2,10 @@
 
 export * from './models/auth.well-known-endpoints';
 export * from './models/authorization-result.enum';
+export * from './models/authorization-state.enum';
 export * from './models/jwtkeys';
 export * from './models/validate-state-result.model';
+export * from './models/validation-result.enum.model';
 export * from './modules/auth.configuration';
 export * from './modules/auth.module';
 export * from './services/oidc.security.config.service';
