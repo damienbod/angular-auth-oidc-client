68f54bc6c5f9f9cfb90d50ec189bf7dd3535b779
damienbod <damien_bod@hotmail.com>
2018-11-20 17:42:03 +0100

    make it possible to turn the iss validation off per configuration

--- src/modules/auth.configuration.ts
+++ src/modules/auth.configuration.ts
@@ -30,6 +30,7 @@ export class OpenIDImplicitFlowConfiguration {
     trigger_authorization_result_event = false;
     log_console_warning_active = true;
     log_console_debug_active = false;
+    iss_validation_off = false;
 
     // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
     // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
@@ -202,6 +203,16 @@ export class AuthConfiguration {
 
         return this.defaultConfig.log_console_debug_active;
     }
+
+    get iss_validation_off(): boolean {
+        if (this.openIDImplicitFlowConfiguration) {
+            return this.openIDImplicitFlowConfiguration.iss_validation_off;
+        }
+
+        return this.defaultConfig.iss_validation_off;
+    }
+
+    
 
     get max_id_token_iat_offset_allowed_in_seconds(): number {
         if (this.openIDImplicitFlowConfiguration) {
