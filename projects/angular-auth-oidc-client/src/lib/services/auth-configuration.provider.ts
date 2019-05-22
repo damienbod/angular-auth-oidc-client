fcd8ec0d310caf620ecac481d03abd606c7a968f
damienbod <damien_bod@hotmail.com>
2019-05-22 07:36:18 +0200

    build fix

--- projects/angular-auth-oidc-client/src/lib/services/auth-configuration.provider.ts
+++ projects/angular-auth-oidc-client/src/lib/services/auth-configuration.provider.ts
@@ -35,7 +35,18 @@ export class ConfigurationProvider {
     };
 
     private mergedOpenIdConfiguration: OpenIdInternalConfiguration = this.DEFAULT_CONFIG;
-    private authWellKnownEndpoints: AuthWellKnownEndpoints;
+    private authWellKnownEndpoints: AuthWellKnownEndpoints = {
+        issuer: '',
+        jwks_uri: '',
+        authorization_endpoint: '',
+        token_endpoint: '',
+        userinfo_endpoint: '',
+        end_session_endpoint: '',
+        check_session_iframe: '',
+        revocation_endpoint: '',
+        introspection_endpoint: ''
+    };
+
     private onConfigurationChangeInternal = new Subject<OpenIdConfiguration>();
 
     get openIDConfiguration(): OpenIdInternalConfiguration {
