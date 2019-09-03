0a6696bc4b5a83783ccfe367a0f0601224cef5af
damienbod <damien_bod@hotmail.com>
2019-09-03 10:22:02 +0200

    tests

--- projects/angular-auth-oidc-client/src/tests/services/url-parser.service.spec.ts
+++ projects/angular-auth-oidc-client/src/tests/services/url-parser.service.spec.ts
@@ -26,4 +26,31 @@ describe('ConfigurationProviderTests', () => {
         expect(code).toBe('thisisacode');
         expect(state).toBe('0000.1234.000');
     });
+	
+	it('parses Url correctly 2', () => {
+        const urlToCheck = 'https://www.example.com/signin?code=thisisa$-_.+!*(),code&state=0000.1234.000#';
+        const code = service.getUrlParameter(urlToCheck, 'code');
+        const state = service.getUrlParameter(urlToCheck, 'state');
+
+        expect(code).toBe('thisisa$-_.+!*(),code');
+        expect(state).toBe('0000.1234.000');
+    });
+	
+	it('parses Url correctly 3', () => {
+        const urlToCheck = 'https://www.example.com/signin?code=thisisacode&state=0000.1234.000';
+        const code = service.getUrlParameter(urlToCheck, 'code');
+        const state = service.getUrlParameter(urlToCheck, 'state');
+
+        expect(code).toBe('thisisacode');
+        expect(state).toBe('0000.1234.000');
+    });
+	
+	it('parses Url correctly negative', () => {
+        const urlToCheck = 'https://www.example.com/signin?state=0000.1234.000&ui_locales=de&code=thisisacode#lang=de';
+        const code = service.getUrlParameter(urlToCheck, 'code');
+        const state = service.getUrlParameter(urlToCheck, 'state');
+
+        expect(code).toBe('thisisacode');
+        expect(state).toBe('0000.1234.000');
+    });
 });
