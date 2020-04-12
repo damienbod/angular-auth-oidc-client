a25a2d093be23ec6f1a325a94654a28848df214c
damienbod <damien_bod@hotmail.com>
2020-04-12 16:29:59 +0200

    adding security stuff

--- projects/sample-code-flow-auto-login/src/app/app.module.ts
+++ projects/sample-code-flow-auto-login/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 
+export function loadConfig(oidcConfigService: OidcConfigService) {
+  return () => oidcConfigService.load_using_stsServer('https://offeringsolutions-sts.azurewebsites.net');
+}
+
 @NgModule({
     declarations: [
       AppComponent,
@@ -32,7 +36,40 @@ import { HomeComponent } from './home/home.component';
           { path: 'forbidden', component: AppComponent },
           { path: 'unauthorized', component: AppComponent },
       ])],
-    providers: [],
+      providers: [
+        OidcConfigService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: loadConfig,
+            deps: [OidcConfigService],
+            multi: true,
+        },
+    ],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private oidcSecurityService: OidcSecurityService, private oidcConfigService: OidcConfigService) {
+      this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
+          const config: OpenIdConfiguration = {
+              stsServer: configResult.customConfig.stsServer,
+              redirect_url: 'https://localhost:4200',
+              client_id: 'angularClient',
+              scope: 'openid profile email',
+              response_type: 'code',
+              silent_renew: true,
+              silent_renew_url: 'https://localhost:4200/silent-renew.html',
+              log_console_debug_active: true,
+          };
+
+          // config.start_checksession = true;
+          // config.post_login_route = '/home';
+          // config.forbidden_route = '/home';
+          // config.unauthorized_route = '/home';
+          // config.max_id_token_iat_offset_allowed_in_seconds = 5;
+          // config.history_cleanup_off = true;
+
+          this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
+      });
+  }
+}
+
