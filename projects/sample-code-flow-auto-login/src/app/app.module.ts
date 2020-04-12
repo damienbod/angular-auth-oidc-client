f12b7d9c97d641be0dec03cb8ea254ec7b12e49c
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-04-12 13:10:16 +0200

    imported authmodule form lib w/ cirrect path

--- projects/sample-code-flow-auto-login/src/app/app.module.ts
+++ projects/sample-code-flow-auto-login/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AuthModule } from 'angular-auth-oidc-client';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AuthModule.forRoot()],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
