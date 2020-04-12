526886655b375521f85408d6a1ed0856d4775821
damienbod <damien_bod@hotmail.com>
2020-04-12 16:14:22 +0200

    adding some module stuff

--- projects/sample-code-flow-auto-login/src/app/app.module.ts
+++ projects/sample-code-flow-auto-login/src/app/app.module.ts
@@ -1,7 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { AuthModule } from 'angular-auth-oidc-client';
+import { AuthModule,
+  ConfigResult,
+  OidcConfigService,
+  OidcSecurityService,
+  OpenIdConfiguration } from 'angular-auth-oidc-client';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { AutoLoginComponent } from './auto-login/auto-login.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
@@ -9,8 +16,22 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [AppComponent, ForbiddenComponent, AutoLoginComponent, UnauthorizedComponent, NavigationComponent, HomeComponent],
-    imports: [BrowserModule, AuthModule.forRoot()],
+    declarations: [
+      AppComponent,
+      ForbiddenComponent,
+      AutoLoginComponent,
+      UnauthorizedComponent,
+      NavigationComponent,
+      HomeComponent ],
+    imports: [
+      BrowserModule,
+      HttpClientModule,
+      RouterModule.forRoot([
+          { path: '', component: AppComponent },
+          { path: 'home', component: AppComponent },
+          { path: 'forbidden', component: AppComponent },
+          { path: 'unauthorized', component: AppComponent },
+      ])],
     providers: [],
     bootstrap: [AppComponent],
 })
