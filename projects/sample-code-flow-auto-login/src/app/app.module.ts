ce1539ece1e8bfcdc46c038e244c40f4b84f098c
damienbod <damien_bod@hotmail.com>
2020-04-12 16:06:41 +0200

    adding some components

--- projects/sample-code-flow-auto-login/src/app/app.module.ts
+++ projects/sample-code-flow-auto-login/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuthModule } from 'angular-auth-oidc-client';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { AutoLoginComponent } from './auto-login/auto-login.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ForbiddenComponent, AutoLoginComponent, UnauthorizedComponent, NavigationComponent, HomeComponent],
     imports: [BrowserModule, AuthModule.forRoot()],
     providers: [],
     bootstrap: [AppComponent],
---
+++ projects/sample-code-flow-auto-login/src/app/auto-login/auto-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-login',
+  templateUrl: './auto-login.component.html',
+  styleUrls: ['./auto-login.component.css']
+})
+export class AutoLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
