61384828b7b586d577ba361d3e1bfcbb4cd3e060
damienbod <damien_bod@hotmail.com>
2020-04-14 17:48:19 +0200

    adding Azure AD example

--- projects/sample-implicit-flow-azuread/src/app/app.module.ts
+++ projects/sample-implicit-flow-azuread/src/app/app.module.ts
@@ -1,106 +1,83 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule, ConfigResult, OidcConfigService, OidcSecurityService, OpenIdConfiguration } from 'angular-auth-oidc-client';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-
-import {
-    AuthModule,
-    OidcSecurityService,
-    ConfigResult,
-    OidcConfigService,
-    OpenIdConfiguration
-} from 'angular-auth-oidc-client';
-
-import { AutoLoginComponent } from './auto-login/auto-login.component';
 import { routing } from './app.routes';
-import { ForbiddenComponent } from './forbidden/forbidden.component';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { ProtectedComponent } from './protected/protected.component';
 import { AuthorizationGuard } from './authorization.guard';
-import { environment } from '../environments/environment';
+import { AutoLoginComponent } from './auto-login/auto-login.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+
+//export function loadConfig(oidcConfigService: OidcConfigService) {
+//    console.log('APP_INITIALIZER STARTING');
+//    // https://login.microsoftonline.com/damienbod.onmicrosoft.com/.well-known/openid-configuration
+//    // jwt keys: https://login.microsoftonline.com/common/discovery/keys
+//    // Azure AD does not support CORS, so you need to download the OIDC configuration, and use these from the application.
+//    // The jwt keys needs to be configured in the well-known-openid-configuration.json
+//   return () => oidcConfigService.load(`${window.location.origin}/api/config/configuration`);
+//    //return () => oidcConfigService.load_using_custom_stsServer('https://localhost:44347/well-known-openid-configuration.json');
+//}
 
 export function loadConfig(oidcConfigService: OidcConfigService) {
-  console.log('APP_INITIALIZER STARTING');
-  // https://login.microsoftonline.com/damienbod.onmicrosoft.com/.well-known/openid-configuration
-  // jwt keys: https://login.microsoftonline.com/common/discovery/keys
-  // Azure AD does not support CORS, so you need to download the OIDC configuration, and use these from the application.
-  // The jwt keys needs to be configured in the well-known-openid-configuration.json
-  return () => oidcConfigService.load(`${window.location.origin}/api/config/configuration`);
-  //return () => oidcConfigService.load_using_custom_stsServer('https://localhost:44347/well-known-openid-configuration.json');
+    return () => oidcConfigService.loadUsingStsServer('https://login.microsoftonline.com/damienbod.onmicrosoft.com/v2.0');
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    AutoLoginComponent,
-    ForbiddenComponent,
-    UnauthorizedComponent,
-    ProtectedComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    AuthModule.forRoot(),
-    FormsModule,
-    routing,
-  ],
-  providers: [
-	  OidcSecurityService,
-	  OidcConfigService,
-	  {
-		  provide: APP_INITIALIZER,
-		  useFactory: loadConfig,
-		  deps: [OidcConfigService],
-		  multi: true
-    },
-    AuthorizationGuard
-	],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        AutoLoginComponent,
+        ForbiddenComponent,
+        UnauthorizedComponent,
+        ProtectedComponent,
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        HttpClientModule,
+        AuthModule.forRoot(),
+        FormsModule,
+        routing,
+    ],
+    providers: [
+        OidcSecurityService,
+        OidcConfigService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: loadConfig,
+            deps: [OidcConfigService],
+            multi: true,
+        },
+        AuthorizationGuard,
+    ],
+    bootstrap: [AppComponent],
 })
-
 export class AppModule {
+    constructor(private oidcSecurityService: OidcSecurityService, private oidcConfigService: OidcConfigService) {
+        this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
+            const config: OpenIdConfiguration = {
+                stsServer: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0',
+                redirectUrl: 'https://localhost:44347',
+                clientId: 'e38ea64a-2962-4cde-bfe7-dd2822fdab32',
+                scope: 'openid profile email',
+                responseType: 'id_token token',
+                silentRenew: true,
+                silentRenewUrl: 'https://localhost:44347/silent-renew.html',
+                logConsoleDebugActive: true,
+                maxIdTokenIatOffsetAllowedInSeconds: 500,
+            };
 
-  constructor(
-    private oidcSecurityService: OidcSecurityService,
-    private oidcConfigService: OidcConfigService,
-  ) {
-    this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
+            this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
 
-      const config: OpenIdConfiguration = {
-        stsServer: 'https://login.microsoftonline.com/7ff95b15-dc21-4ba6-bc92-824856578fc1/v2.0/',
-        redirect_url: configResult.customConfig.redirect_url,
-        client_id: configResult.customConfig.client_id,
-        response_type: configResult.customConfig.response_type,
-        scope: configResult.customConfig.scope,
-        post_logout_redirect_uri: configResult.customConfig.post_logout_redirect_uri,
-        start_checksession: configResult.customConfig.start_checksession,
-        silent_renew: configResult.customConfig.silent_renew,
-        silent_renew_url: 'https://localhost:44311/silent-renew.html',
-        post_login_route: '/home',
-        forbidden_route: configResult.customConfig.forbidden_route,
-        unauthorized_route: configResult.customConfig.unauthorized_route,
-        log_console_warning_active: configResult.customConfig.log_console_warning_active,
-        log_console_debug_active: configResult.customConfig.log_console_debug_active,
-        max_id_token_iat_offset_allowed_in_seconds: configResult.customConfig.max_id_token_iat_offset_allowed_in_seconds,
-        auto_userinfo: false,
-        history_cleanup_off: true,
-        iss_validation_off: true
-        // disable_iat_offset_validation: true
-      };
+            this.oidcSecurityService.setCustomRequestParameters(configResult.customConfig.additional_login_parameters);
+            this.oidcSecurityService.setCustomRequestParameters({ response_mode: 'fragment' });
+        });
 
-      this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
-
-      this.oidcSecurityService.setCustomRequestParameters(configResult.customConfig.additional_login_parameters);
-      this.oidcSecurityService.setCustomRequestParameters({ response_mode: 'fragment' } );
-    });
-
-    console.log('APP STARTING');
-  }
+        console.log('APP STARTING');
+    }
 }
