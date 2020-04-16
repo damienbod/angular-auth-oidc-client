9b4e0f3b7f2b39af5f2eb4ff618226a0cb0a754c
damienbod <damien_bod@hotmail.com>
2020-04-16 17:19:31 +0200

    Updating Refresh tokens

--- projects/sample-code-flow-refresh-tokens/src/app/app.component.ts
+++ projects/sample-code-flow-refresh-tokens/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { EventsService, EventTypes, OidcClientNotification, OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'sample-code-flow-refresh-tokens';
+export class AppComponent implements OnInit, OnDestroy {
+    isAuthenticated: boolean;
+    isModuleSetUp$: Observable<OidcClientNotification>;
+    userData: any;
+
+    constructor(public oidcSecurityService: OidcSecurityService, private readonly eventService: EventsService) {
+        this.oidcSecurityService.setupModule();
+        if (this.oidcSecurityService.moduleSetup) {
+            this.doCallbackLogicIfRequired();
+        } else {
+            this.oidcSecurityService.onModuleSetup.subscribe(() => {
+                this.doCallbackLogicIfRequired();
+            });
+        }
+    }
+
+    ngOnInit() {
+        this.isModuleSetUp$ = this.eventService
+            .registerForEvents()
+            .pipe(filter((notification: OidcClientNotification) => notification.type === EventTypes.ModuleSetup));
+
+        this.oidcSecurityService.getIsAuthorized().subscribe((auth) => {
+            this.isAuthenticated = auth;
+        });
+
+        this.oidcSecurityService.getUserData().subscribe((userData) => {
+            this.userData = userData;
+        });
+    }
+
+    ngOnDestroy(): void {}
+
+    login() {
+        this.oidcSecurityService.authorize();
+    }
+
+    logout() {
+        this.oidcSecurityService.logoff();
+    }
+
+    private doCallbackLogicIfRequired() {
+        // Will do a callback, if the url has a code and state parameter.
+        this.oidcSecurityService.authorizedCallbackWithCode(window.location.toString());
+    }
 }
