e5c2ebc4188d745cffcdbf75da9eb639245638c4
damienbod <damien_bod@hotmail.com>
2020-04-16 12:49:53 +0200

    fix silent renew login implicit flow

--- projects/sample-code-flow/src/app/app.component.ts
+++ projects/sample-code-flow/src/app/app.component.ts
@@ -28,7 +28,6 @@ export class AppComponent implements OnInit, OnDestroy {
             .registerForEvents()
             .pipe(filter((notification: OidcClientNotification) => notification.type === EventTypes.ModuleSetup));
 
-        this.oidcSecurityService.setupModule();
         this.oidcSecurityService.getIsAuthorized().subscribe((auth) => {
             this.isAuthenticated = auth;
         });
