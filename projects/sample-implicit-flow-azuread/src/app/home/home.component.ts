3c79842ea1a6ce6d2ea31404be1513e48c2913b1
damienbod <damien_bod@hotmail.com>
2020-04-17 22:33:08 +0200

    updating to user new event data

--- projects/sample-implicit-flow-azuread/src/app/home/home.component.ts
+++ projects/sample-implicit-flow-azuread/src/app/home/home.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService, EventTypes, OidcClientNotification, OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: 'home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    userDataChanged$: Observable<OidcClientNotification>;
+    isAuthenticated: boolean;
+
+    constructor(public oidcSecurityService: OidcSecurityService, private readonly eventsService: EventsService) {}
+
+    ngOnInit() {
+        this.oidcSecurityService.getIsAuthorized().subscribe((auth) => {
+            this.isAuthenticated = auth;
+        });
+
+        this.userDataChanged$ = this.eventsService
+            .registerForEvents()
+            .pipe(filter((notification: OidcClientNotification) => notification.type === EventTypes.UserDataChanged));
+    }
 }
