80e26cb7aa1908bdae2a075eea358d0349f72e0e
damienbod <damien_bod@hotmail.com>
2020-04-18 08:36:46 +0200

    adding support for IE

--- projects/angular-auth-oidc-client/src/lib/utils/random/random.service.ts
+++ projects/angular-auth-oidc-client/src/lib/utils/random/random.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 
-// support for IE, change to (window.crypto || window.msCrypto)
+// support for IE,  (window.crypto || window.msCrypto)
 @Injectable({ providedIn: 'root' })
 export class RandomService {
     createRandom(requiredLength: number): string {
         const length = requiredLength - 6;
         const arr = new Uint8Array((length || length) / 2);
-        window.crypto.getRandomValues(arr);
+        (window.crypto || (window as any).msCrypto).getRandomValues(arr);
         return Array.from(arr, this.toHex).join('') + this.randomString(7);
     }
 
@@ -19,7 +19,7 @@ export class RandomService {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
         const values = new Uint32Array(length);
-        window.crypto.getRandomValues(values);
+        (window.crypto || (window as any).msCrypto).getRandomValues(values);
         for (let i = 0; i < length; i++) {
             result += characters[values[i] % characters.length];
         }
