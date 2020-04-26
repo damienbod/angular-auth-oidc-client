f12540642f360bca1bd75dea8b4d4c4c4bb65728
damienbod <damien_bod@hotmail.com>
2020-04-26 13:46:01 +0200

    Updating Azure AD and Azure B2C examples

--- projects/sample-implicit-flow-ad-b2c/src/app/app.component.ts
+++ projects/sample-implicit-flow-ad-b2c/src/app/app.component.ts
@@ -1,36 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-    AuthorizationResult,
-    AuthorizedState,
-    EventsService,
-    EventTypes,
-    OidcClientNotification,
-    OidcSecurityService,
-} from 'angular-auth-oidc-client';
-import { filter, tap } from 'rxjs/operators';
+import { EventsService, OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+    templateUrl: 'app.component.html',
 })
-export class AppComponent implements OnInit {
-    title = 'app';
-
+export class AppComponent implements OnInit, OnDestroy {
     constructor(public oidcSecurityService: OidcSecurityService, private router: Router, private readonly eventsService: EventsService) {}
 
     ngOnInit() {
-        this.eventsService
-            .registerForEvents()
-            .pipe(filter((notification: OidcClientNotification<any>) => notification.type === EventTypes.NewAuthorizationResult))
-            .subscribe((authorizationResult) => this.onAuthorizationResultComplete(authorizationResult.value));
-
         this.oidcSecurityService
             .checkAuth()
-            .pipe(tap(() => this.onOidcModuleSetup()))
-            .subscribe((isAuthenticated) => console.log('i am ', isAuthenticated));
+
+            .subscribe((isAuthenticated) => {
+                if (!isAuthenticated) {
+                    if ('/autologin' !== window.location.pathname) {
+                        this.write('redirect', window.location.pathname);
+                        this.router.navigate(['/autologin']);
+                    }
+                }
+                if (isAuthenticated) {
+                    this.navigateToStoredEndpoint();
+                }
+            });
     }
+
+    ngOnDestroy(): void {}
 
     login() {
         console.log('start login');
@@ -47,32 +43,17 @@ export class AppComponent implements OnInit {
         this.oidcSecurityService.logoff();
     }
 
-    private onOidcModuleSetup() {
-        if (window.location.hash) {
-            this.oidcSecurityService.authorizedImplicitFlowCallback().subscribe();
-        } else {
-            if ('/autologin' !== window.location.pathname) {
-                this.write('redirect', window.location.pathname);
-            }
-            console.warn('AppComponent:onOidcModuleSetup false' + window.location.pathname);
-            this.oidcSecurityService.isAuthenticated$.subscribe((authorized: boolean) => {
-                if (!authorized) {
-                    this.router.navigate(['/autologin']);
-                }
-            });
-        }
-    }
-
-    private onAuthorizationResultComplete(authorizationResult: AuthorizationResult) {
+    private navigateToStoredEndpoint() {
         const path = this.read('redirect');
-        console.log('AppComponent:onAuthorizationResultComplete path: ' + path);
-        if (authorizationResult.authorizationState === AuthorizedState.Authorized) {
-            if (path) {
-                console.log('AppComponent:onAuthorizationResultComplete go to path ' + path);
-                this.router.navigate([path]);
-            }
+
+        if (this.router.url === path) {
+            return;
+        }
+
+        if (path.toString().includes('/unauthorized')) {
+            this.router.navigate(['/']);
         } else {
-            this.router.navigate(['/unauthorized']);
+            this.router.navigate([path]);
         }
     }
 
