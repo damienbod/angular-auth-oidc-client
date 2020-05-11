cfab947131ccbd1f7b57db04d4c48f38c573c507
damienbod <damien_bod@hotmail.com>
2020-05-11 16:08:38 +0200

    fix tests

--- projects/angular-auth-oidc-client/src/lib/config/config.provider.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/config/config.provider.spec.ts
@@ -125,6 +125,7 @@ describe('ConfigurationProviderTests', () => {
             disableIatOffsetValidation: false,
             storage: sessionStorage,
             customParams: {},
+            disableRefreshIdTokenAuthTimeValidation: false,
         };
 
         configurationProvider.setConfig(config, null);
@@ -167,6 +168,7 @@ describe('ConfigurationProviderTests', () => {
             disableIatOffsetValidation: false,
             storage: sessionStorage,
             customParams: {},
+            disableRefreshIdTokenAuthTimeValidation: false,
         };
 
         spyOnProperty(platformProvider, 'isBrowser').and.returnValue(false);
