c455c97ba61170fa2e6d8e523cf2d8fda0d09f7a
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-05-18 08:04:05 +0200

    adding flows tests

--- projects/angular-auth-oidc-client/src/lib/flows/flows.service.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/flows/flows.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { DataService } from '../api/data.service';
 import { DataServiceMock } from '../api/data.service-mock';
 import { AuthStateService } from '../authState/auth-state.service';
@@ -17,6 +18,7 @@ import { StateValidationService } from '../validation/state-validation.service';
 import { StateValidationServiceMock } from '../validation/state-validation.service-mock';
 import { TokenValidationService } from '../validation/token-validation.service';
 import { TokenValidationServiceMock } from '../validation/token-validation.service-mock';
+import { CallbackContext } from './callback-context';
 import { FlowsDataService } from './flows-data.service';
 import { FlowsDataServiceMock } from './flows-data.service-mock';
 import { FlowsService } from './flows.service';
@@ -25,6 +27,10 @@ import { SigninKeyDataServiceMock } from './signin-key-data.service-mock';
 
 describe('Flows Service', () => {
     let service: FlowsService;
+    let configurationProvider: ConfigurationProvider;
+    let userService: UserService;
+    let flowsDataService: FlowsDataService;
+    let authStateService: AuthStateService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -47,9 +53,120 @@ describe('Flows Service', () => {
 
     beforeEach(() => {
         service = TestBed.inject(FlowsService);
+        configurationProvider = TestBed.inject(ConfigurationProvider);
+        userService = TestBed.inject(UserService);
+        flowsDataService = TestBed.inject(FlowsDataService);
+        authStateService = TestBed.inject(AuthStateService);
     });
 
     it('should create', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('resetAuthorizationData', () => {
+        it('calls resetUserDataInStore when autoUserInfo is true', () => {
+            spyOnProperty(configurationProvider, 'openIDConfiguration').and.returnValue({ autoUserinfo: true });
+            const resetUserDataInStoreSpy = spyOn(userService, 'resetUserDataInStore');
+            spyOn(flowsDataService, 'resetStorageFlowData');
+            spyOn(authStateService, 'setUnauthorizedAndFireEvent');
+
+            service.resetAuthorizationData();
+
+            expect(resetUserDataInStoreSpy).toHaveBeenCalled();
+        });
+
+        it('does not call resetUserDataInStore when autoUserInfo is false', () => {
+            spyOnProperty(configurationProvider, 'openIDConfiguration').and.returnValue({ autoUserinfo: false });
+            const resetUserDataInStoreSpy = spyOn(userService, 'resetUserDataInStore');
+            spyOn(flowsDataService, 'resetStorageFlowData');
+            spyOn(authStateService, 'setUnauthorizedAndFireEvent');
+
+            service.resetAuthorizationData();
+
+            expect(resetUserDataInStoreSpy).not.toHaveBeenCalled();
+        });
+
+        it('calls correct methods', () => {
+            spyOnProperty(configurationProvider, 'openIDConfiguration').and.returnValue({ autoUserinfo: false });
+            const resetStorageFlowDataSpy = spyOn(flowsDataService, 'resetStorageFlowData');
+            const setUnauthorizedAndFireEventSpy = spyOn(authStateService, 'setUnauthorizedAndFireEvent');
+
+            service.resetAuthorizationData();
+
+            expect(resetStorageFlowDataSpy).toHaveBeenCalled();
+            expect(setUnauthorizedAndFireEventSpy).toHaveBeenCalled();
+        });
+    });
+
+    describe('processCodeFlowCallback', () => {
+        it('calls all methods correctly', async(() => {
+            const codeFlowCallbackSpy = spyOn(service as any, 'codeFlowCallback').and.returnValue(of({}));
+            const codeFlowCodeRequestSpy = spyOn(service as any, 'codeFlowCodeRequest').and.returnValue(of({}));
+            const callbackHistoryAndResetJwtKeysSpy = spyOn(service as any, 'callbackHistoryAndResetJwtKeys').and.returnValue(of({}));
+            const callbackStateValidationSpy = spyOn(service as any, 'callbackStateValidation').and.returnValue(of({}));
+            const callbackUserSpy = spyOn(service as any, 'callbackUser').and.returnValue(of({}));
+
+            service.processCodeFlowCallback('some-url').subscribe((value) => {
+                expect(value).toBeTruthy();
+                expect(codeFlowCallbackSpy).toHaveBeenCalledWith('some-url');
+                expect(codeFlowCodeRequestSpy).toHaveBeenCalled();
+                expect(callbackHistoryAndResetJwtKeysSpy).toHaveBeenCalled();
+                expect(callbackStateValidationSpy).toHaveBeenCalled();
+                expect(callbackUserSpy).toHaveBeenCalled();
+            });
+        }));
+    });
+
+    describe('processSilentRenewCodeFlowCallback', () => {
+        it('calls all methods correctly', async(() => {
+            const codeFlowCodeRequestSpy = spyOn(service as any, 'codeFlowCodeRequest').and.returnValue(of({}));
+            const callbackHistoryAndResetJwtKeysSpy = spyOn(service as any, 'callbackHistoryAndResetJwtKeys').and.returnValue(of({}));
+            const callbackStateValidationSpy = spyOn(service as any, 'callbackStateValidation').and.returnValue(of({}));
+            const callbackUserSpy = spyOn(service as any, 'callbackUser').and.returnValue(of({}));
+
+            service.processSilentRenewCodeFlowCallback({} as CallbackContext).subscribe((value) => {
+                expect(value).toBeTruthy();
+                expect(codeFlowCodeRequestSpy).toHaveBeenCalled();
+                expect(callbackHistoryAndResetJwtKeysSpy).toHaveBeenCalled();
+                expect(callbackStateValidationSpy).toHaveBeenCalled();
+                expect(callbackUserSpy).toHaveBeenCalled();
+            });
+        }));
+    });
+
+    describe('processImplicitFlowCallback', () => {
+        it('calls all methods correctly', async(() => {
+            const implicitFlowCallbackSpy = spyOn(service as any, 'implicitFlowCallback').and.returnValue(of({}));
+            const callbackHistoryAndResetJwtKeysSpy = spyOn(service as any, 'callbackHistoryAndResetJwtKeys').and.returnValue(of({}));
+            const callbackStateValidationSpy = spyOn(service as any, 'callbackStateValidation').and.returnValue(of({}));
+            const callbackUserSpy = spyOn(service as any, 'callbackUser').and.returnValue(of({}));
+
+            (service as any).processImplicitFlowCallback('any-hash').subscribe((value) => {
+                expect(value).toBeTruthy();
+                expect(implicitFlowCallbackSpy).toHaveBeenCalled();
+                expect(callbackHistoryAndResetJwtKeysSpy).toHaveBeenCalled();
+                expect(callbackStateValidationSpy).toHaveBeenCalled();
+                expect(callbackUserSpy).toHaveBeenCalled();
+            });
+        }));
+    });
+
+    describe('processRefreshToken', () => {
+        it('calls all methods correctly', async(() => {
+            const refreshSessionWithRefreshTokensSpy = spyOn(service as any, 'refreshSessionWithRefreshTokens').and.returnValue(of({}));
+            const refreshTokensRequestTokensSpy = spyOn(service as any, 'refreshTokensRequestTokens').and.returnValue(of({}));
+            const callbackHistoryAndResetJwtKeysSpy = spyOn(service as any, 'callbackHistoryAndResetJwtKeys').and.returnValue(of({}));
+            const callbackStateValidationSpy = spyOn(service as any, 'callbackStateValidation').and.returnValue(of({}));
+            const callbackUserSpy = spyOn(service as any, 'callbackUser').and.returnValue(of({}));
+
+            (service as any).processRefreshToken().subscribe((value) => {
+                expect(value).toBeTruthy();
+                expect(refreshSessionWithRefreshTokensSpy).toHaveBeenCalled();
+                expect(refreshTokensRequestTokensSpy).toHaveBeenCalled();
+                expect(callbackHistoryAndResetJwtKeysSpy).toHaveBeenCalled();
+                expect(callbackStateValidationSpy).toHaveBeenCalled();
+                expect(callbackUserSpy).toHaveBeenCalled();
+            });
+        }));
     });
 });
