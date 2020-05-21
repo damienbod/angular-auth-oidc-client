bda3615e3699f7828093dfa4c7a82a34a42c7b6f
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-05-21 17:00:43 +0200

    fixed auth well known tests

--- projects/angular-auth-oidc-client/src/lib/config/auth-well-known-data.service.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/config/auth-well-known-data.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { DataService } from '../api/data.service';
 import { DataServiceMock } from '../api/data.service-mock';
 import { AuthWellKnownDataService } from './auth-well-known-data.service';
@@ -23,30 +24,48 @@ describe('AuthWellKnownDataService', () => {
     });
 
     describe('getWellKnownDocument', () => {
-        // it('should add suffix if it does not exist on current url', () => {
-        //     const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
-        //         return of(null);
-        //     });
-        //     const urlWithoutSuffix = 'myUrl';
-        //     const urlWithSuffix = `${urlWithoutSuffix}/.well-known/openid-configuration`;
-        //     service.getWellKnownDocument(urlWithoutSuffix);
-        //     expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
-        // });
-        // it('should not add suffix if it does exist on current url', () => {
-        //     const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
-        //         return of(null);
-        //     });
-        //     const urlWithSuffix = `myUrl/.well-known/openid-configuration`;
-        //     service.getWellKnownDocument(urlWithSuffix);
-        //     expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
-        // });
-        // it('should not add suffix if it does exist in the middle of current url', () => {
-        //     const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
-        //         return of(null);
-        //     });
-        //     const urlWithSuffix = `myUrl/.well-known/openid-configuration/and/some/more/stuff`;
-        //     service.getWellKnownDocument(urlWithSuffix);
-        //     expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
-        // });
+        it('should add suffix if it does not exist on current url', async(() => {
+            const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
+                return of(null);
+            });
+            const urlWithoutSuffix = 'myUrl';
+            const urlWithSuffix = `${urlWithoutSuffix}/.well-known/openid-configuration`;
+            (service as any).getWellKnownDocument(urlWithoutSuffix).subscribe(() => {
+                expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
+            });
+        }));
+
+        it('should not add suffix if it does exist on current url', async(() => {
+            const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
+                return of(null);
+            });
+            const urlWithSuffix = `myUrl/.well-known/openid-configuration`;
+            (service as any).getWellKnownDocument(urlWithSuffix).subscribe(() => {
+                expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
+            });
+        }));
+
+        it('should not add suffix if it does exist in the middle of current url', async(() => {
+            const dataServiceSpy = spyOn(dataService, 'get').and.callFake((url) => {
+                return of(null);
+            });
+            const urlWithSuffix = `myUrl/.well-known/openid-configuration/and/some/more/stuff`;
+            (service as any).getWellKnownDocument(urlWithSuffix).subscribe(() => {
+                expect(dataServiceSpy).toHaveBeenCalledWith(urlWithSuffix);
+            });
+        }));
+    });
+
+    describe('getWellKnownEndPointsFromUrl', () => {
+        it('calling internal getWellKnownDocument and maps', () => {
+            spyOn<any>(dataService, 'get').and.returnValue(of({ jwks_uri: 'jwks_uri' }));
+
+            const spy = spyOn(service as any, 'getWellKnownDocument').and.callThrough();
+            service.getWellKnownEndPointsFromUrl('any-url').subscribe((result) => {
+                expect(spy).toHaveBeenCalled();
+                expect((result as any).jwks_uri).toBeUndefined();
+                expect(result.jwksUri).toBe('jwks_uri');
+            });
+        });
     });
 });
