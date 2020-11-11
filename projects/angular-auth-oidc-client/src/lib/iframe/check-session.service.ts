33f50203c218a4eb06c5414bd2aa6945660d762e
Roman Yakym <roman.yakym@traxgo.be>
2020-11-11 11:50:58 +0200

    Add checkSessionChanged event emmiting when session state becomes blank to notify about it all already opened tabs.

--- projects/angular-auth-oidc-client/src/lib/iframe/check-session.service.ts
+++ projects/angular-auth-oidc-client/src/lib/iframe/check-session.service.ts
@@ -109,6 +109,7 @@ export class CheckSessionService {
                             );
                         } else {
                             this.loggerService.logDebug('OidcSecurityCheckSession pollServerSession session_state is blank');
+                            this.checkSessionChanged$.next();
                         }
                     } else {
                         this.loggerService.logWarning('OidcSecurityCheckSession pollServerSession checkSession IFrame does not exist');
