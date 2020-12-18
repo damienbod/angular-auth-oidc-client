b15db7f885ecc8677720b67b8fa8f476fac01034
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-12-18 19:17:10 +0100

    next try

--- projects/angular-auth-oidc-client/karma.conf.js
+++ projects/angular-auth-oidc-client/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
         coverageReporter: {
             dir: require('path').join(__dirname, '../../coverage/angular-auth-oidc-client'),
             subdir: '.',
-            reporters: [{ type: 'html' }, { type: 'text-summary' }],
+            reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcov' }],
         },
         reporters: ['progress', 'kjhtml'],
         port: 9876,
