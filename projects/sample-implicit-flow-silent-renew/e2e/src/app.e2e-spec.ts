b45f8d50a04e47859d2c02468285f762c3622542
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-12-18 10:20:19 +0100

    step four

--- projects/sample-implicit-flow-silent-renew/e2e/src/app.e2e-spec.ts
+++ projects/sample-implicit-flow-silent-renew/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('sample-implicit-flow-silent-renew app is running!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('sample-implicit-flow-silent-renew app is running!');
   });
 
   afterEach(async () => {
