6cba0ae9354964b023420dbba2ef847bd5980ed5
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-12-28 15:02:04 +0100

    Merge branch 'main' into fabiangosebrink/adding-schematics

--- projects/schematics/src/utils/angular-utils.ts
+++ projects/schematics/src/utils/angular-utils.ts
@@ -1,5 +1,5 @@
-import { experimental } from '@angular-devkit/core';
 import { SchematicsException, Tree } from '@angular-devkit/schematics';
+import { WorkspaceProject } from '@schematics/angular/utility/workspace-models';
 import ts = require('@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript');
 export const ANGULAR_JSON_FILENAME = 'angular.json';
 
@@ -19,7 +19,7 @@ export function getAngularWorkspace(tree: Tree) {
 export function getProject(tree: Tree, projectName?: string) {
     const workspace = getAngularWorkspace(tree);
     const project = !!projectName ? workspace.projects[projectName] : workspace.defaultProject;
-    return workspace.projects[project] as experimental.workspace.WorkspaceProject;
+    return workspace.projects[project] as WorkspaceProject;
 }
 
 export function readIntoSourceFile(host: Tree, fileName: string): ts.SourceFile {
