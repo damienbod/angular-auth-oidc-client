de55c249549f020226397c67a5b9b3f71dab3a70
damienbod <damien_bod@hotmail.com>
2021-02-13 17:44:13 +0100

    release 11.5.1

--- projects/schematics/src/ng-add/actions/add-dependencies.ts
+++ projects/schematics/src/ng-add/actions/add-dependencies.ts
@@ -4,7 +4,7 @@ import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@s
 const dependenciesToAdd = [
   {
     name: 'angular-auth-oidc-client',
-    version: '11.5.0',
+    version: '11.5.1',
   },
 ];
 
