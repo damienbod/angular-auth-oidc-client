1d11074a36d8d75c7b5070ba4e818e8849a33f73
damienbod <damien_bod@hotmail.com>
2021-02-27 16:06:31 +0100

    Add this to the DOCS, redirect path to an auto login guard

--- projects/sample-code-flow-azuread/src/app/app.routes.ts
+++ projects/sample-code-flow-azuread/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ProtectedComponent } from './protected/protected.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent, canActivate: [AutoLoginGuard] },
   { path: 'forbidden', component: ForbiddenComponent, canActivate: [AutoLoginGuard] },
   { path: 'protected', component: ProtectedComponent, canActivate: [AutoLoginGuard] },
