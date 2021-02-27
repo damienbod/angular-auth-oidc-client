4ee8ba691b508eb21b2d30f423e06cb7e3b4e2ca
damienbod <damien_bod@hotmail.com>
2021-02-27 16:27:44 +0100

    Updating Azure B2C example

--- projects/sample-code-flow-azure-b2c/src/app/app.component.ts
+++ projects/sample-code-flow-azure-b2c/src/app/app.component.ts
@@ -1,57 +1,10 @@
-﻿import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+﻿import { Component } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
 })
-export class AppComponent implements OnInit, OnDestroy {
-  constructor(public oidcSecurityService: OidcSecurityService, private router: Router) {}
-
-  ngOnInit() {
-    this.oidcSecurityService
-      .checkAuth()
-
-      .subscribe((isAuthenticated) => {
-        if (!isAuthenticated) {
-          if ('/autologin' !== window.location.pathname) {
-            this.write('redirect', window.location.pathname);
-            this.router.navigate(['/autologin']);
-          }
-        }
-        if (isAuthenticated) {
-          this.navigateToStoredEndpoint();
-        }
-      });
-  }
-
-  ngOnDestroy(): void {}
-
-  private navigateToStoredEndpoint() {
-    const path = this.read('redirect');
-
-    if (this.router.url === path) {
-      return;
-    }
-
-    if (path.toString().includes('/unauthorized')) {
-      this.router.navigate(['/']);
-    } else {
-      this.router.navigate([path]);
-    }
-  }
-
-  private read(key: string): any {
-    const data = localStorage.getItem(key);
-    if (data != null) {
-      return JSON.parse(data);
-    }
-
-    return;
-  }
-
-  private write(key: string, value: any): void {
-    localStorage.setItem(key, JSON.stringify(value));
-  }
+export class AppComponent {
+  constructor(public oidcSecurityService: OidcSecurityService) {}
 }
