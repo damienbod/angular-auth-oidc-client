1b684921452868d2a8a4e89253936231b48360a3
damienbod <damien_bod@hotmail.com>
2021-05-14 19:55:00 +0200

    Updating auth0 settings to include the access token for the api

--- projects/sample-code-flow-multi-Auth0-ID4/src/app/auth-config.module.ts
+++ projects/sample-code-flow-multi-Auth0-ID4/src/app/auth-config.module.ts
@@ -17,18 +17,18 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
           logLevel: LogLevel.Debug,
         },
         {
-		  stsServer: 'https://dev-damienbod.eu.auth0.com',
-		  redirectUrl: window.location.origin,
-		  postLogoutRedirectUri: window.location.origin,
-		  clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
-		  scope: 'openid profile offline_access auth0-user-api-spa',
-		  responseType: 'code',
-		  silentRenew: true,
-		  useRefreshToken: true,
-		  logLevel: LogLevel.Debug,
-		  customParams: {
-			"audience": "https://auth0-api-spa"
-		  }
+          stsServer: 'https://dev-damienbod.eu.auth0.com',
+          redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
+          scope: 'openid profile offline_access auth0-user-api-spa',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+          customParams: {
+            audience: 'https://auth0-api-spa',
+          },
         },
       ],
     }),
