80ff13d73cee33dc6f6bb93c33c69b22b73d4e54
damienbod <damien_bod@hotmail.com>
2021-05-15 14:53:28 +0200

    Adding an AAD mutli config

---
+++ projects/sample-code-flow-multi-AAD/src/app/auth-config.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
+
+@NgModule({
+  imports: [
+    AuthModule.forRoot({
+      config: [
+        {
+          stsServer: 'https://offeringsolutions-sts.azurewebsites.net',
+          redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'angularCodeRefreshTokens',
+          scope: 'openid profile email taler_api offline_access',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+        },
+        {
+          stsServer: 'https://dev-damienbod.eu.auth0.com',
+          redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
+          scope: 'openid profile offline_access auth0-user-api-spa',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+          customParams: {
+            audience: 'https://auth0-api-spa',
+          },
+        },
+      ],
+    }),
+  ],
+  exports: [AuthModule],
+})
+export class AuthConfigModule {}
---
+++ projects/sample-code-flow-multi-AAD/src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { OidcClientNotification, OidcSecurityService, OpenIdConfiguration } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.component.html',
+})
+export class HomeComponent implements OnInit {
+  configurations: OpenIdConfiguration[];
+  userDataChanged$: Observable<OidcClientNotification<any>>;
+  userData$: Observable<any>;
+  isAuthenticated$: Observable<any>;
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.configurations = this.oidcSecurityService.getConfigurations();
+    this.userData$ = this.oidcSecurityService.userData$;
+    this.isAuthenticated$ = this.oidcSecurityService.isAuthenticated$;
+  }
+
+  login(configId: string) {
+    this.oidcSecurityService.authorize(configId);
+  }
+
+  forceRefreshSession() {
+    this.oidcSecurityService.forceRefreshSession().subscribe((result) => console.warn(result));
+  }
+
+  logout(configId: string) {
+    this.oidcSecurityService.logoff(configId);
+  }
+
+  refreshSession(configId: string) {
+    this.oidcSecurityService.forceRefreshSession(null, configId).subscribe((result) => console.log(result));
+  }
+
+  logoffAndRevokeTokens(configId: string) {
+    this.oidcSecurityService.logoffAndRevokeTokens(null, configId).subscribe((result) => console.log(result));
+  }
+
+  revokeRefreshToken(configId: string) {
+    this.oidcSecurityService.revokeRefreshToken(null, configId).subscribe((result) => console.log(result));
+  }
+}
