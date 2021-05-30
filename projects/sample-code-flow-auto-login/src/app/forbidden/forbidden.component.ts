80061c42d2e064d56fdd4c9358de11e5e14371a0
damienbod <damien_bod@hotmail.com>
2021-05-30 21:03:02 +0200

    build fixes auto complete

--- projects/sample-code-flow-auto-login/src/app/forbidden/forbidden.component.ts
+++ projects/sample-code-flow-auto-login/src/app/forbidden/forbidden.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-forbidden',
   templateUrl: 'forbidden.component.html',
 })
 export class ForbiddenComponent implements OnInit {
-  public authenticated$: Observable<boolean>;
+  public isAuthenticated = false;
 
   constructor(private oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
-    this.authenticated$ = this.oidcSecurityService.isAuthenticated$;
+    this.oidcSecurityService.isAuthenticated$.subscribe((authenticated: boolean) => {
+      this.isAuthenticated = authenticated;
+
+      console.warn('authenticated: ', authenticated);
+    });
   }
 }
