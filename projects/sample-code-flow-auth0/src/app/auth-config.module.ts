477ecb72ffb4c47c501b699e02a7b35a6b2a9c85
damienbod <damien_bod@hotmail.com>
2021-05-30 14:56:32 +0200

    fixing auth0 sample

--- projects/sample-code-flow-auth0/src/app/auth-config.module.ts
+++ projects/sample-code-flow-auth0/src/app/auth-config.module.ts
@@ -1,37 +1,27 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { AuthModule, LogLevel, OidcConfigService } from 'angular-auth-oidc-client';
-
-export function configureAuth(oidcConfigService: OidcConfigService) {
-  return () =>
-    oidcConfigService.withConfig({
-      stsServer: 'https://dev-damienbod.eu.auth0.com',
-      redirectUrl: window.location.origin,
-      postLogoutRedirectUri: window.location.origin,
-      clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
-      scope: 'openid profile offline_access auth0-user-api-spa',
-      responseType: 'code',
-      silentRenew: true,
-      useRefreshToken: true,
-      logLevel: LogLevel.Debug,
-      customParamsAuthRequest: {
-        audience: 'https://auth0-api-spa',
-      },
-      customParamsRefreshTokenRequest: {
-        scope: 'openid profile offline_access auth0-user-api-spa',
-      },
-    });
-}
+import { NgModule } from '@angular/core';
+import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 
 @NgModule({
-  imports: [AuthModule.forRoot()],
-  providers: [
-    OidcConfigService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: configureAuth,
-      deps: [OidcConfigService],
-      multi: true,
-    },
+  imports: [
+    AuthModule.forRoot({
+      config: {
+        stsServer: 'https://dev-damienbod.eu.auth0.com',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: 'Ujh5oSBAFr1BuilgkZPcMWEgnuREgrwU',
+        scope: 'openid profile offline_access auth0-user-api-spa',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        logLevel: LogLevel.Debug,
+        customParamsAuthRequest: {
+          audience: 'https://auth0-api-spa',
+        },
+        customParamsRefreshTokenRequest: {
+          scope: 'openid profile offline_access auth0-user-api-spa',
+        },
+      },
+    }),
   ],
   exports: [AuthModule],
 })
