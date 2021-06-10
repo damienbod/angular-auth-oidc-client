80bb331acbe1ded9bb104db27b6cca46fdb6b045
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-06-10 17:21:46 +0200

    reverted auto login example

--- projects/sample-code-flow-auto-login/src/app/app.routes.ts
+++ projects/sample-code-flow-auto-login/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent, canActivate: [AutoLoginGuard] },
+  { path: 'home', component: HomeComponent },
   { path: 'protected', component: ProtectedComponent, canActivate: [AutoLoginGuard] },
   { path: 'forbidden', component: ForbiddenComponent, canActivate: [AutoLoginGuard] },
   {
