061873eddd5f50827171306a2a05b11dbafb0e82
damienbod <damien_bod@hotmail.com>
2021-06-12 22:59:24 +0200

    init sign-in flow

--- projects/sample-code-flow-multi-Azure-B2C/src/app/auth-config.module.ts
+++ projects/sample-code-flow-multi-Azure-B2C/src/app/auth-config.module.ts
@@ -11,7 +11,7 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
             'https://b2cdamienbod.b2clogin.com/b2cdamienbod.onmicrosoft.com/B2C_1_sign_in/v2.0/.well-known/openid-configuration',
           redirectUrl: window.location.origin,
           postLogoutRedirectUri: window.location.origin,
-          clientId: 'dbbcae9e-4c14-4b05-a862-4e773699248a',
+          clientId: '00b1b58e-6193-4be1-bf15-3a37d6adefaf',
           scope: 'openid profile https://b2cdamienbod.onmicrosoft.com/ac9b845d-96d3-4410-9923-50ec7bc80db9/access_as_user',
           responseType: 'code',
           silentRenew: true,
@@ -27,7 +27,7 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
           authWellknownEndpointUrl:
             'https://b2cdamienbod.b2clogin.com/b2cdamienbod.onmicrosoft.com/B2C_1_sign_in/v2.0/.well-known/openid-configuration',
           redirectUrl: window.location.origin,
-          clientId: 'dbbcae9e-4c14-4b05-a862-4e773699248a',
+          clientId: '00b1b58e-6193-4be1-bf15-3a37d6adefaf',
           scope: 'openid profile User.ReadBasic.All',
           responseType: 'code',
           silentRenew: true,
