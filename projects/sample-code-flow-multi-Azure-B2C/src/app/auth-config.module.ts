e9b0b833405a6a694fd2047a9bf285dc41e52747
damienbod <damien_bod@hotmail.com>
2021-06-12 23:21:49 +0200

    updating with 2 b2c apis

--- projects/sample-code-flow-multi-Azure-B2C/src/app/auth-config.module.ts
+++ projects/sample-code-flow-multi-Azure-B2C/src/app/auth-config.module.ts
@@ -27,8 +27,9 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
           authWellknownEndpointUrl:
             'https://b2cdamienbod.b2clogin.com/b2cdamienbod.onmicrosoft.com/B2C_1_sign_in/v2.0/.well-known/openid-configuration',
           redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
           clientId: '00b1b58e-6193-4be1-bf15-3a37d6adefaf',
-          scope: 'openid profile User.ReadBasic.All',
+          scope: 'openid profile https://b2cdamienbod.onmicrosoft.com/c41bf62e-73e6-4f5d-bed5-70ba1b2c8284/access_as_user',
           responseType: 'code',
           silentRenew: true,
           useRefreshToken: true,
