bc8af57c5b517263a0a35ae78cbd2fd1b4cd44ab
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-06-19 10:04:33 +0200

    First running example

--- projects/sample-code-flow-auto-login-all-routes/src/app/app.module.ts
+++ projects/sample-code-flow-auto-login-all-routes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthConfigModule } from './auth-config.module';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
