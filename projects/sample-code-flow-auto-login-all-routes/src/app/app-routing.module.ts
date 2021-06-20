c9de1ab7e32fc93e7ad45ec42bfaac26d197163e
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2021-06-20 15:03:33 +0200

    Fixed samples

--- projects/sample-code-flow-auto-login-all-routes/src/app/app-routing.module.ts
+++ projects/sample-code-flow-auto-login-all-routes/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AutoLoginCompleteGuard } from 'angular-auth-oidc-client';
+import { AutoLoginAllRoutesGuard } from 'angular-auth-oidc-client';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { HomeComponent } from './home/home.component';
 import { ProtectedComponent } from './protected/protected.component';
@@ -8,13 +8,13 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent, canActivate: [AutoLoginCompleteGuard] },
-  { path: 'protected', component: ProtectedComponent, canActivate: [AutoLoginCompleteGuard] },
-  { path: 'forbidden', component: ForbiddenComponent, canActivate: [AutoLoginCompleteGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AutoLoginAllRoutesGuard] },
+  { path: 'protected', component: ProtectedComponent, canActivate: [AutoLoginAllRoutesGuard] },
+  { path: 'forbidden', component: ForbiddenComponent, canActivate: [AutoLoginAllRoutesGuard] },
   {
     path: 'customers',
     loadChildren: () => import('./customers/customers.module').then((m) => m.CustomersModule),
-    canLoad: [AutoLoginCompleteGuard],
+    canLoad: [AutoLoginAllRoutesGuard],
   },
   { path: 'unauthorized', component: UnauthorizedComponent },
 ];
