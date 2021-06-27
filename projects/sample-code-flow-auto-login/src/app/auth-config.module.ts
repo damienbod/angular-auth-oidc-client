e250d4ed703dbebb325d8bbaa464a9d05046733b
damienbod <damien_bod@hotmail.com>
2021-06-27 16:07:24 +0200

    switching auto-login example to refresh tokens

--- projects/sample-code-flow-auto-login/src/app/auth-config.module.ts
+++ projects/sample-code-flow-auto-login/src/app/auth-config.module.ts
@@ -5,23 +5,20 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        stsServer: 'https://offeringsolutions-sts.azurewebsites.net',
-        redirectUrl: window.location.origin,
-        clientId: 'angularJwtClient',
-        scope: 'openid profile email',
-        responseType: 'code',
         triggerAuthorizationResultEvent: true,
-        postLogoutRedirectUri: `${window.location.origin}/unauthorized`,
-        startCheckSession: false,
-        silentRenew: true,
-        silentRenewUrl: `${window.location.origin}/silent-renew.html`,
         postLoginRoute: '/home',
         forbiddenRoute: '/forbidden',
         unauthorizedRoute: '/unauthorized',
         logLevel: LogLevel.Debug,
         historyCleanupOff: true,
-        // iss_validation_off: false
-        // disable_iat_offset_validation: true
+        stsServer: 'https://offeringsolutions-sts.azurewebsites.net',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: 'angularCodeRefreshTokens',
+        scope: 'openid profile email offline_access',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
       },
     }),
   ],
