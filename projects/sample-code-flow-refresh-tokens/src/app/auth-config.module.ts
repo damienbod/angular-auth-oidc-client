8a25d5d59f4601350a835beb9cdfda0706b42427
Fredrik Dyrvold <fredrik@struqtur.se>
2021-07-22 16:59:09 +0200

    config v1

--- projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
+++ projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
@@ -5,16 +5,21 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        authority: 'https://offeringsolutions-sts.azurewebsites.net',
+        authority: 'https://login.microsoftonline.com/94999c36-9100-4153-9cbc-ddce798f5c95/v2.0/.well-known/openid-configuration', ///94999c36-9100-4153-9cbc-ddce798f5c95/oauth2/v2.0
         redirectUrl: window.location.origin,
         postLogoutRedirectUri: window.location.origin,
-        clientId: 'angularCodeRefreshTokens',
-        scope: 'openid profile email taler_api offline_access',
+        clientId: 'bf171d26-c5df-4da7-b2e6-d30f39c11863',
+        scope: 'openid profile',
         responseType: 'code',
-        // autoUserInfo: true,
         silentRenew: true,
         useRefreshToken: true,
         logLevel: LogLevel.Debug,
+        maxIdTokenIatOffsetAllowedInSeconds: 600,
+        issValidationOff: false,
+        autoUserInfo: false,
+        customParamsAuthRequest: {
+          prompt: 'select_account', // login, consent
+        },
       },
     }),
   ],
