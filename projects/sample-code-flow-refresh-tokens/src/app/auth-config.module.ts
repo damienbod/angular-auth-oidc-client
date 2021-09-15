63ec58c2535ece32d3dad919f0a59ba76c6027c7
Fredrik Dyrvold <fredrik@struqtur.se>
2021-09-15 16:30:29 +0200

    config changes

--- projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
+++ projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
@@ -4,25 +4,29 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 @NgModule({
   imports: [
     AuthModule.forRoot({
-      config: {
-        authority: 'https://login.microsoftonline.com/94999c36-9100-4153-9cbc-ddce798f5c95/v2.0/.well-known/openid-configuration', ///94999c36-9100-4153-9cbc-ddce798f5c95/oauth2/v2.0
-        redirectUrl: window.location.origin,
-        postLogoutRedirectUri: window.location.origin,
-        clientId: 'bf171d26-c5df-4da7-b2e6-d30f39c11863',
-        scope: 'openid profile',
-        responseType: 'code',
-        silentRenew: true,
-        useRefreshToken: true,
-        logLevel: LogLevel.Debug,
-        maxIdTokenIatOffsetAllowedInSeconds: 600,
-        issValidationOff: false,
-        autoUserInfo: false,
-        customParamsAuthRequest: {
-          prompt: 'select_account', // login, consent
+      config: [
+        {
+          authority: 'https://login.microsoftonline.com/94999c36-9100-4153-9cbc-ddce798f5c95/v2.0/.well-known/openid-configuration', ///94999c36-9100-4153-9cbc-ddce798f5c95/oauth2/v2.0
+          redirectUrl: window.location.origin,
+          postLogoutRedirectUri: window.location.origin,
+          clientId: 'bf171d26-c5df-4da7-b2e6-d30f39c11863',
+          scope: 'openid profile',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          logLevel: LogLevel.Debug,
+          maxIdTokenIatOffsetAllowedInSeconds: 600,
+          issValidationOff: false,
+          autoUserInfo: false,
+          customParamsAuthRequest: {
+            prompt: 'select_account', // login, consent
+          },
         },
-      },
+
+      ],
     }),
   ],
   exports: [AuthModule],
 })
-export class AuthConfigModule {}
+export class AuthConfigModule {
+}
