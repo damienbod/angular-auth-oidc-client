250197205f3b8b75333b9c0ba6baff778730ac93
Jie Lin <linjie997@gmail.com>
2021-11-30 09:30:33 +0100

    Fix formatting quotes

--- projects/angular-auth-oidc-client/src/lib/validation/jsrsasign-reduced.service.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/validation/jsrsasign-reduced.service.spec.ts
@@ -1,17 +1,15 @@
-import { TestBed } from "@angular/core/testing";
-import { JsrsAsignReducedService } from "./jsrsasign-reduced.service";
-import { TestScheduler } from "rxjs/testing";
-import { Observable } from "rxjs";
+import { TestBed } from '@angular/core/testing';
+import { JsrsAsignReducedService } from './jsrsasign-reduced.service';
+import { TestScheduler } from 'rxjs/testing';
 
-describe("JsrsAsignReducedService", () => {
+describe('JsrsAsignReducedService', () => {
   let service: JsrsAsignReducedService;
   let testScheduler: TestScheduler;
-  let actions: Observable<any>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [],
-      providers: [JsrsAsignReducedService]
+      providers: [JsrsAsignReducedService],
     });
   });
 
@@ -22,14 +20,14 @@ describe("JsrsAsignReducedService", () => {
     });
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(service).toBeTruthy();
   });
 
-  describe("generateCodeChallenge", () => {
-    it("returns good result with correct codeVerifier", () => {
-      const outcome = "R2TWD45Vtcf_kfAqjuE3LMSRF3JDE5fsFndnn6-a0nQ";
-      const observable = service.generateCodeChallenge("44445543344242132145455aaabbdc3b4");
+  describe('generateCodeChallenge', () => {
+    it('returns good result with correct codeVerifier', () => {
+      const outcome = 'R2TWD45Vtcf_kfAqjuE3LMSRF3JDE5fsFndnn6-a0nQ';
+      const observable = service.generateCodeChallenge('44445543344242132145455aaabbdc3b4');
 
       observable.subscribe(value => {
         expect(value).toBe(outcome);
