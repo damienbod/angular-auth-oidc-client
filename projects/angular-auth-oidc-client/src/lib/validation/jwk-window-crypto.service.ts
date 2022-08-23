8f03006a6f05ce5d4ab60231cd4650829acf2ae0
Erich Json Fosse <erich@halloverden.no>
2022-08-24 00:42:58 +0200

    ADD: Method for verifying key

--- projects/angular-auth-oidc-client/src/lib/validation/jwk-window-crypto.service.ts
+++ projects/angular-auth-oidc-client/src/lib/validation/jwk-window-crypto.service.ts
@@ -8,4 +8,8 @@ export class JwkWindowCryptoService {
   importVerificationKey(key: JsonWebKey, algorithm: AlgorithmIdentifier | RsaHashedImportParams | EcKeyImportParams | HmacImportParams | AesKeyAlgorithm): Promise<CryptoKey> {
     return this.cryptoService.getCrypto().subtle.importKey('jwk', key, algorithm, false, ['verify']);
   }
+
+  verifyKey(verifyAlgorithm: AlgorithmIdentifier | RsaPssParams | EcdsaParams, cryptoKey: CryptoKey, signature: BufferSource, signingInput: string): Promise<boolean> {
+    return this.cryptoService.getCrypto().subtle.verify(verifyAlgorithm, cryptoKey, signature, new TextEncoder().encode(signingInput))
+  }
 }
