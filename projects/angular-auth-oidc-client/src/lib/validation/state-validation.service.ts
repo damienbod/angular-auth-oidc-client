90bd63be0236e94516777c1e6d94bf2b6f1c137b
damienbod <damien_bod@hotmail.com>
2022-11-05 22:26:57 +0100

    Remove id_token in validation in refresh token flow, id_token only needs to be refreshed in initial authentication validation

--- projects/angular-auth-oidc-client/src/lib/validation/state-validation.service.ts
+++ projects/angular-auth-oidc-client/src/lib/validation/state-validation.service.ts
@@ -114,6 +114,7 @@ export class StateValidationService {
           }
 
           if (
+            !isInRefreshTokenFlow &&
             !this.tokenValidationService.validateIdTokenIatMaxOffset(
               toReturn.decodedIdToken,
               maxIdTokenIatOffsetAllowedInSeconds,
