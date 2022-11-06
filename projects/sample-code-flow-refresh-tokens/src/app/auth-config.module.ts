16e130d25d1b1aeb55d804e645609c215dd08cf8
damienbod <damien_bod@hotmail.com>
2022-11-06 11:22:11 +0100

    reset default test app

--- projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
+++ projects/sample-code-flow-refresh-tokens/src/app/auth-config.module.ts
@@ -5,17 +5,15 @@ import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        authority: 'https://devsso.vermeer.com/nidp',
+        authority: 'https://offeringsolutions-sts.azurewebsites.net',
         redirectUrl: window.location.origin,
         postLogoutRedirectUri: window.location.origin,
-        clientId: '58df815e-41a1-4fbb-b628-e7c4f8d46e48',
-        scope: 'openid profile offline_access',
+        clientId: 'angularCodeRefreshTokens',
+        scope: 'openid profile email taler_api offline_access',
         responseType: 'code',
         silentRenew: true,
         useRefreshToken: true,
-        ignoreNonceAfterRefresh: true,
-        triggerRefreshWhenIdTokenExpired: false,
-        autoUserInfo: false,
+        // triggerRefreshWhenIdTokenExpired: false,
         logLevel: LogLevel.Debug,
       },
     }),
