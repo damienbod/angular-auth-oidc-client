2dacfc3dfad31d03c98ce153dff3a235be5d1e94
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-12-24 10:32:03 +0100

    fix: support Angular 14

--- projects/angular-auth-oidc-client/src/lib/provide-auth.ts
+++ projects/angular-auth-oidc-client/src/lib/provide-auth.ts
@@ -1,10 +1,15 @@
-import { EnvironmentProviders, makeEnvironmentProviders, Provider } from '@angular/core';
+import { makeEnvironmentProviders, Provider } from '@angular/core';
 import { createStaticLoader, PassedInitialConfig, PASSED_CONFIG } from './auth-config';
 import { StsConfigLoader } from './config/loader/config-loader';
 import { AbstractLoggerService } from './logging/abstract-logger.service';
 import { ConsoleLoggerService } from './logging/console-logger.service';
 import { AbstractSecurityStorage } from './storage/abstract-security-storage';
 import { DefaultSessionStorageService } from './storage/default-sessionstorage.service';
+
+// this type can be dropped when Angular 14 support is dropped (and imported from angular/core)
+declare type EnvironmentProviders = {
+  ɵbrand: 'EnvironmentProviders';
+};
 
 export function provideAuth(passedConfig: PassedInitialConfig): EnvironmentProviders {
   return makeEnvironmentProviders([..._provideAuth(passedConfig)]);
