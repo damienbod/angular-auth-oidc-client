92eb426cf3ea21408fada4877e1747adc0201b34
bclementfidx <128393626+bclementfidx@users.noreply.github.com>
2023-03-20 10:45:37 -0500

    remove unused variable

--- projects/angular-auth-oidc-client/src/lib/auth-state/check-auth.service.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/auth-state/check-auth.service.spec.ts
@@ -7,7 +7,6 @@ import { CallbackService } from '../callback/callback.service';
 import { PeriodicallyTokenCheckService } from '../callback/periodically-token-check.service';
 import { RefreshSessionService } from '../callback/refresh-session.service';
 import { StsConfigLoader, StsConfigStaticLoader } from '../config/loader/config-loader';
-import { OpenIdConfiguration } from '../config/openid-configuration';
 import { CheckSessionService } from '../iframe/check-session.service';
 import { SilentRenewService } from '../iframe/silent-renew.service';
 import { LoggerService } from '../logging/logger.service';
@@ -150,7 +149,6 @@ describe('CheckAuthService', () => {
       spyOn(popUpService as any, 'canAccessSessionStorage').and.returnValue(true);
       spyOnProperty(popUpService as any, 'windowInternal').and.returnValue({ opener: {} as Window });
       spyOn(storagePersistenceService, 'read').and.returnValue(null);
-      const config = {} as OpenIdConfiguration;
 
       spyOn(popUpService, 'isCurrentlyInPopup').and.returnValue(true);
       const popupSpy = spyOn(popUpService, 'sendMessageToMainWindow');
