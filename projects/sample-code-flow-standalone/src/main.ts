9905727dfc02ae8f691b027a595857c256626106
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-05-29 13:15:25 +0200

    refactor: extract config to file

--- projects/sample-code-flow-standalone/src/main.ts
+++ projects/sample-code-flow-standalone/src/main.ts
@@ -1,35 +1,6 @@
-import {importProvidersFrom} from "@angular/core";
-import {bootstrapApplication} from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import {AutoLoginPartialRoutesGuard} from "angular-auth-oidc-client";
+import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import {HomeComponent} from "./app/home/home.component";
-import {ProtectedComponent} from "./app/protected/protected.component";
-import {ForbiddenComponent} from "./app/forbidden/forbidden.component";
-import {UnauthorizedComponent} from "./app/unauthorized/unauthorized.component";
-import {AuthConfigModule} from "./app/auth-config.module";
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(AuthConfigModule),
-    provideRouter(
-      [
-        { path: '', pathMatch: 'full', redirectTo: 'home' },
-        { path: 'home', component: HomeComponent },
-        { path: 'protected', component: ProtectedComponent, canActivate: [AutoLoginPartialRoutesGuard] },
-        { path: 'forbidden', component: ForbiddenComponent, canActivate: [AutoLoginPartialRoutesGuard] },
-        {
-          path: 'customers',
-          loadChildren: () => import('./app/customers/customers.routes').then((m) => m.routes),
-          canLoad: [AutoLoginPartialRoutesGuard],
-        },
-        { path: 'unauthorized', component: UnauthorizedComponent },
-      ],
-      withEnabledBlockingInitialNavigation()
-    ),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
