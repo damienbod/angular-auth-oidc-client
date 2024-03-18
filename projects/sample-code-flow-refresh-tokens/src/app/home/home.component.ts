8e6f6e12e21d51542b40e9a60b2f09e5507f5d6c
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-03-18 10:08:06 +0100

    Refactor code to remove redundant assignments and imports

--- projects/sample-code-flow-refresh-tokens/src/app/home/home.component.ts
+++ projects/sample-code-flow-refresh-tokens/src/app/home/home.component.ts
@@ -1,27 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  OidcClientNotification,
-  OidcSecurityService,
-  OpenIdConfiguration,
-  UserDataResult,
-} from 'angular-auth-oidc-client';
-import { Observable } from 'rxjs';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
 })
 export class HomeComponent implements OnInit {
-  configuration$: Observable<OpenIdConfiguration>;
-  userDataChanged$: Observable<OidcClientNotification<any>>;
-  userData$: Observable<UserDataResult>;
+  configuration$ = this.oidcSecurityService.getConfiguration();
+  userData$ = this.oidcSecurityService.userData$;
   isAuthenticated = false;
   constructor(public oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
-    this.configuration$ = this.oidcSecurityService.getConfiguration();
-    this.userData$ = this.oidcSecurityService.userData$;
-
     this.oidcSecurityService.isAuthenticated$.subscribe(
       ({ isAuthenticated }) => {
         this.isAuthenticated = isAuthenticated;
