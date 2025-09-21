ecb65663e2073b4cfef00069383909f825ad5a1e
natecolbertonsport <nate@onsport.ai>
2025-09-21 16:55:55 -0700

    fixed document import

--- projects/angular-auth-oidc-client/src/lib/utils/redirect/redirect.service.spec.ts
+++ projects/angular-auth-oidc-client/src/lib/utils/redirect/redirect.service.spec.ts
@@ -1,4 +1,4 @@
-import { DOCUMENT } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
 import { RedirectService } from './redirect.service';
 
